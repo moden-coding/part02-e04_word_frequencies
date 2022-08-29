5d57159339335bee901310ada8d1ebdd59794015
modengann <99417989+modengann@users.noreply.github.com>
2022-08-29 11:50:57 -0400

    Update word_frequencies.py

--- src/word_frequencies.py
+++ src/word_frequencies.py
@@ -2,20 +2,7 @@
 
 def word_frequencies(filename):
     
-    with open(filename, "r") as f:
-        words_freq = {}
-        for line in f:
-            temp = line.split()
-            for word in temp:
-                word = clean(word)
-                if word in words_freq:
-                    words_freq[word] = words_freq[word] + 1
-                else:
-                    words_freq[word] = 1
-        return words_freq
-                
-    
-    
+  
     return {}
 
 def main():
